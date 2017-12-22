c2a7b01999a19db5b0459e2a0261e7ceb7a5ad3d
Padale <q1qnbk@splm.siemens.com>
2017-12-22 18:17:10 +0530

    actuals

---
+++ sample 1.py
@@ -0,0 +1,53 @@
+"""
+@wi EL-139 EL-137 main module comment
+This is a multiline comment for the document. This docstring comment describes requirements for our python parser.
+
+- The format of a single requirement claim shall be the following: Req LSA-\d+
+- The format of multiple requirement claims shall be the following: Req LSA-\d+ LSA-\d+ LSA-\d+ LSA-\d+ ...
+-- Example: Req LSA-0001 LSA-0002 LSA-0003
+
+- The parser shall support tracing to entire files.
+-- All instances of the requirement format found in a file-level docstring shall trace that file to that requirement.
+
+- The parser shall support tracing to functions.
+-- All instances of the requirement format found in a function-level docstring shall trace the requirement to that function and file
+
+- The parser shall support tracing to lines.
+-- All instances of the requirement format found in a line-level comment shall trace the requirement to that line and file
+
+
+The following line is an example of a claim to this file
+
+@wi LSA-1234
+
+"""
+
+class MyClass:
+	"""@wi EL-139 MyClass DocString comment """
+    # @wi EL-139 MyClass 2nd comment
+	a = 10
+    # @wi EL-139 MyClass 3nd comment
+	def myFunc(self):
+        # @wi EL-139 myFunc DocString comment
+        # asda decode
+        # @wi EL-139 myFunc 2nd comment
+		print('Hello')
+
+def myFunc2(self):
+    # @wi EL-139 myFunc2 DocString comment
+    print('Hello')
+
+"""asdasdas"""
+    
+# extra middle comment
+print(MyClass.a)
+
+# @wi EL-139 random statement comment
+print(MyClass.func)
+
+
+# extra last comment
+print(MyClass.__doc__)
+
+#
+class sample:
