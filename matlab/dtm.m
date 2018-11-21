48b785bcabf36d15ca7397ffa74ce47eb74b8a94
ankurspadale <ankurspadale@gmail.com>
2018-11-21 16:33:29 +0530

    matlab files

---
+++ matlab/dtm.m
@@ -0,0 +1,14 @@
+%  Discrete Fourier Transform
+% @wi APPL-422 optional comment 
+function Xk = dft(x) 
+[N,M] = size(x);
+if M ~=1,   % @wi APPL-423 makes sure that x is a column vector
+  x = x';
+  N = M;
+end
+Xk=zeros(N,1);
+n = 0:N-1;
+for k=0:N-1
+  Xk(k+1) = exp(-j*2*pi*k*n/N)*x;
+end
+end
